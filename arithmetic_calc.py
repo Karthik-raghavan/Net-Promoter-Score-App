625ff8bdddad621b9c488c4a32705e22a8902747
@karthikraghavan <132128134+Karthik-raghavan@users.noreply.github.com>
2024-06-03 11:51:34 +0530

    Update arithmetic_calc.py

--- arithmetic_calc.py
+++ arithmetic_calc.py
@@ -1,14 +1,14 @@
 def add(x,y):
-    return x + y
+    pass
 
 def subtract(x,y):
-    return x-y
+    pass
 
 def multiply(x*y):
-    return x * y
+    pass
 
 def divide(x,y):
-    return x/y
+    pass
 
 def square(x):
-    return x ** 2
+    pass
