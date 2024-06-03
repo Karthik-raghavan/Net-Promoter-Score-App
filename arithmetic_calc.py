b9f9e6904133e5745ce0913dbc2d70f635ed1e05
Karthik-Raghavan <karthikraghavan@icloud.com>
2024-06-03 11:20:43 +0530

    Created the arithmetic python file

---
+++ arithmetic_calc.py
@@ -0,0 +1,14 @@
+def add(x,y):
+    return x + y
+
+def subtract(x,y):
+    return x-y
+
+def multiply(x*y):
+    return x * y
+
+def divide(x,y):
+    return x / y
+
+def square(x):
+    return x ** 2
